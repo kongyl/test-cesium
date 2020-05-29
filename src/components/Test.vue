4be7ea58825fd614eb79ed78353703dcfd4c533c
kongyl <kongyl@126.com>
2020-05-29 16:04:56 +0800

    add arcgis layer

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="cesiumContainer"></div>
+  <div id="cesiumContainer">
+    <div id="credit" hidden></div>
+  </div>  
 </template>
 
 <script>
@@ -14,7 +16,35 @@ export default {
     }
   },
   mounted() {
-    this.viewer = new Cesium.Viewer('cesiumContainer');
+    var baseLayer = new Cesium.TileMapServiceImageryProvider({
+      url: Cesium.buildModuleUrl('Assets/Textures/NaturalEarthII')
+    });
+    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5ODNjMmQ2Mi1hMTBlLTQxM2YtODBkMS1iM2MzNmY3OTFkZmYiLCJpZCI6NDAwNiwic2NvcGVzIjpbImFzciJdLCJhc3NldHMiOlsxXSwiaWF0IjoxNTg2ODY5MjcyfQ.lFy5UGAmDp7qcarXUrBG7K4mMJwCXal8on3UrLNQEHk';
+    this.viewer = new Cesium.Viewer('cesiumContainer', {
+      animation:false,
+      baseLayerPicker:false,
+      fullscreenButton:false,
+      geocoder:false,
+      homeButton:false,
+      infoBox:false,
+      sceneModePicker:false,
+      selectionIndicator:false,
+      timeline:false,
+      navigationHelpButton:false,
+      navigationInstructionsInitiallyVisible:false,
+      navigation: true,
+      terrainProvider: Cesium.createWorldTerrain(),
+      creditContainer: 'credit',
+      imageryProvider: baseLayer
+    });
+    var rsLayer = new Cesium.ArcGisMapServerImageryProvider({
+      url: 'http://28h413242y.wicp.vip/arcgis/rest/services/仲恺/MapServer',
+      usePreCachedTilesIfAvailable: false
+    });
+    this.viewer.imageryLayers.addImageryProvider(rsLayer);
+    this.viewer.scene.camera.setView({
+      destination: Cesium.Cartesian3.fromDegrees(114.345661, 23.040161, 10000.0)
+    });
   }
 }
 </script>
